711ed176ca2f8ecb357255a42a7e0c20e03282f1
bigguy345 <monkeybar3200@gmail.com>
2025-06-06 02:59:10 +0300

    - Ctrl-Inserts Rinne Tomoe into empty slot

--- src/main/java/net/narutomod/goatee/network/packets/NarutoWheelData.java
+++ src/main/java/net/narutomod/goatee/network/packets/NarutoWheelData.java
@@ -66,7 +66,7 @@ public final class NarutoWheelData extends AbstractPacket {
             boolean swapWithHelmet = false;
             ItemStack toSwapWith = player.getHeldItemMainhand().getItem() instanceof ItemDojutsu.Base ? player.getHeldItemMainhand() : (swapWithHelmet = helmet.getItem() instanceof ItemDojutsu.Base) ? helmet : ItemStack.EMPTY;
 
-            if (helmet == toSwapWith && ItemRinneganTomoe.isTomoe(helmet)&& !ItemRinnegan.isRinnesharinganActivated(helmet))
+            if (helmet == toSwapWith && ItemRinneganTomoe.isTomoe(helmet)&& !ItemRinnegan.isRinnesharinganActivated(helmet) && !toInventory)
                 ItemRinneganTomoe.setTomoeStatus(helmet, ItemRinneganTomoe.SHARINGAN_OFF_STATUS,player);
             else if (!toSwapWith.isEmpty()) {
                 seg.putToSaved(toSwapWith, seg.stack);
