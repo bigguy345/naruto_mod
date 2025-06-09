27b70add581e8f837f4ae8ad66e9588a8fa2acff
bigguy345 <monkeybar3200@gmail.com>
2025-06-09 04:56:27 +0300

    - Set target's marker name to target's name if they have a Hiraishin Kunai

--- src/main/java/net/narutomod/item/ItemKunaiHiraishin.java
+++ src/main/java/net/narutomod/item/ItemKunaiHiraishin.java
@@ -168,7 +168,10 @@ public class ItemKunaiHiraishin extends ElementsNarutomodMod.ModElement {
 					if (lastMarkerUuid != null && !lastMarkerUuid.equals(entity.getUniqueID())) {
 						EntityHiraishin.updateServerMarkerMap(ownerUuid, lastMarkerUuid, null);
 					}
-					EntityHiraishin.updateServerMarkerMap(ownerUuid, entity.getUniqueID(), new EntityHiraishin.MarkerData(new Vector4d(entity.posX, entity.posY, entity.posZ, entity.dimension)));
+					EntityHiraishin.MarkerData data = new EntityHiraishin.MarkerData(new Vector4d(entity.posX, entity.posY, entity.posZ, entity.dimension));
+					data.name = entity.getDisplayName().getFormattedText();
+					//data.targetId = entity.getEntityId();
+					EntityHiraishin.updateServerMarkerMap(ownerUuid, entity.getUniqueID(), data);
 					stack.getTagCompound().setUniqueId("lastMarkerUuid", entity.getUniqueID());
 				} else if (lastMarkerUuid != null) {
 					EntityHiraishin.updateServerMarkerMap(ownerUuid, lastMarkerUuid, null);
