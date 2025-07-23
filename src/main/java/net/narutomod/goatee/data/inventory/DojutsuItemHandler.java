100ab7abd5af29aba43f4ddc563fedbbb3bfb9c1
bigguy345 <monkeybar3200@gmail.com>
2025-07-23 03:40:40 +0300

    Dojutsu Slot now applies attribute modifiers and fires equip event

--- src/main/java/net/narutomod/goatee/data/inventory/DojutsuItemHandler.java
+++ src/main/java/net/narutomod/goatee/data/inventory/DojutsuItemHandler.java
@@ -1,6 +1,10 @@
 package net.narutomod.goatee.data.inventory;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.items.ItemStackHandler;
 import net.narutomod.item.ItemDojutsu;
 
@@ -14,5 +18,24 @@ public class DojutsuItemHandler extends ItemStackHandler {
     @Override
     public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
         return stack.getItem() instanceof ItemDojutsu.Base;
+    }
+
+    public void setSlot(ItemStack stack, EntityPlayer player) {
+        ItemStack prev = getStackInSlot(0);
+        setStackInSlot(0, stack);
+        if (!player.world.isRemote) {
+            if (ItemStack.areItemStacksEqual(prev, stack))
+                return;
+            
+            MinecraftForge.EVENT_BUS.post(new LivingEquipmentChangeEvent(player, EntityEquipmentSlot.HEAD, prev, stack));
+
+            if (!prev.isEmpty()) {
+                player.getAttributeMap().removeAttributeModifiers(prev.getAttributeModifiers(EntityEquipmentSlot.HEAD));
+            }
+
+            if (!stack.isEmpty()) {
+                player.getAttributeMap().applyAttributeModifiers(stack.getAttributeModifiers(EntityEquipmentSlot.HEAD));
+            }
+        }
     }
 }
